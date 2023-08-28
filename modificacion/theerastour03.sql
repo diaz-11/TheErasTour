1f14565b2c3f5551389a5fc4cc103d6eedcff56f
diaz-11 <lucasdomin62@gmail.com>
2023-08-28 14:25:49 -0300

    insertar

---
+++ modificacion/theerastour03.sql
@@ -0,0 +1,10 @@
+INSERT INTO user (username, password)
+VALUES ("taylor swift",eras321)
+("fearless","karma01"),
+ ("speak now", "sparks fly3" ),
+ ("red", "all to well"),
+ ("1989","style"),
+ ("reputation", ),
+ ("lover","cruel sommer"),
+ ("folklore"),("evermore, ""), 
+ ("midnights", "inti hero ")
